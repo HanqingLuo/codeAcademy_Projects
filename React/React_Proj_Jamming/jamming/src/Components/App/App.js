0cf49dd8f8524d7a2f0d8c113d3308b96ee018b6
Hanqing Luo <kukuman520@gmail.com>
2021-04-06 17:41:18 -0400

    App.js render()

--- React/React_Proj_Jamming/jamming/src/Components/App/App.js
+++ React/React_Proj_Jamming/jamming/src/Components/App/App.js
@@ -1,24 +1,21 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+          <SearchBar />
+          <div class="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
