167d9a023596778a702f6cee0393fc1281418fe4
Hanqing Luo <kukuman520@gmail.com>
2021-04-07 10:38:00 -0400

    24

---
+++ React/React_Proj_Jamming/jamming/src/Components/TrackList/TrackList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TrackList.css';
+
+class TrackList extends React.Component {
+
+    render() {
+        return (
+            <div className="TrackList">
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+            </div>
+        )
+    }
+
+}
+
+export default TrackList;
