8e103b6dec786093e55b95f194c2ec75ae872b34
Hanqing Luo <kukuman520@gmail.com>
2021-04-07 15:47:57 -0400

    31

--- React/React_Proj_Jamming/jamming/src/Components/App/App.js
+++ React/React_Proj_Jamming/jamming/src/Components/App/App.js
@@ -7,8 +7,13 @@ import Playlist from '../Playlist/Playlist';
 
 
 class App extends React.Component {
-  constructor(props){
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      SearchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
+      { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
+      { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }]
+    }
 
   }
   render() {
