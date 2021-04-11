1308142ac1ef4b0312dcc167e75cf1149895adaf
Hanqing Luo <kukuman520@gmail.com>
2021-04-11 16:14:00 -0400

    46

--- React/React_Proj_Jamming/jamming/src/Components/Track/Track.js
+++ React/React_Proj_Jamming/jamming/src/Components/Track/Track.js
@@ -6,6 +6,12 @@ import './Track.css';
 
 
 class Track extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+    }
+
     renderAction() {
         if (this.props.isRemoval) {
             return <button className="Track-action" >-</button>
@@ -18,7 +24,7 @@ class Track extends React.Component {
     addTrack(){
         this.props.onAdd(this.props.track);
     }
-    
+
     render() {
         return (
             <div className="Track">
