400260b581f34061940b8dcf41eb96225cc74a6a
Hanqing Luo <kukuman520@gmail.com>
2021-04-12 19:25:44 -0400

    86

--- React/React_Proj_Jamming/jamming/src/util/Spotify.js
+++ React/React_Proj_Jamming/jamming/src/util/Spotify.js
@@ -26,8 +26,14 @@ const Spotify = {
         }
     },
 
-    search(term){
-        
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+            {
+                headers: { Authorization: `Bearer ${accessToken}` }
+
+            }
+        )
     }
 
 
