51f653dad176ca334fdb4f56494cfafdf3502d14
Hanqing Luo <kukuman520@gmail.com>
2021-04-12 19:14:03 -0400

    80

--- React/React_Proj_Jamming/jamming/src/util/Spotify.js
+++ React/React_Proj_Jamming/jamming/src/util/Spotify.js
@@ -1,14 +1,22 @@
 let accessToken = ""
 
 const Spotify = {
-    getAccessToken(){
-        if(accessToken){
+    getAccessToken() {
+        if (accessToken) {
             return accessToken;
         }
 
         // check for access token match
-        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            // This clears the parameters, allowing us to grab a new access token when it expires.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+        }
     }
 }
 
