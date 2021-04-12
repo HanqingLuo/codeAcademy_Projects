56cafdacfe511ecbd94ac1c28256a869c8b904bd
Hanqing Luo <kukuman520@gmail.com>
2021-04-12 19:45:45 -0400

    94

--- React/React_Proj_Jamming/jamming/src/util/Spotify.js
+++ React/React_Proj_Jamming/jamming/src/util/Spotify.js
@@ -68,6 +68,11 @@ const Spotify = {
                 }).then(response => response.json()
                 ).then(jsonResponse =>{
                     const playlistId = jsonResponse.id;
+                    return fetch(`/v1/users/${userId}/playlists/${playlistId}/tracks`,{
+                        headers: headers,
+                        method: 'POST',
+                        body: JSON.stringify({uris:trackUris})
+                    })
                 })
         })
     }
